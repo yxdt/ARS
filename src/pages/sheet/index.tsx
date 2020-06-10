96e905b428ebdeda146bc0b3f1dcfd510b88bdc9
yxdt <blueterry@yahoo.com>
2020-06-10 14:56:46 +0800

    waybill

--- src/pages/sheet/index.tsx
+++ src/pages/sheet/index.tsx
@@ -1,9 +1,27 @@
 import Taro, { Component, Config } from "@tarojs/taro";
-import { View, Text, Image, Button } from "@tarojs/components";
-import { AtNavBar, AtIcon } from "taro-ui";
+import { View, Text, Picker } from "@tarojs/components";
+import {
+  AtNavBar,
+  AtForm,
+  AtInput,
+  AtList,
+  AtListItem,
+  AtButton,
+} from "taro-ui";
 import "./index.scss";
 
 export default class Index extends Component {
+  constructor() {
+    super(...arguments);
+    this.state = {
+      userName: "",
+      cellphone: "",
+      platenumber: "",
+      trucktype: "",
+      checked: false,
+    };
+  }
+
   componentWillMount() {}
 
   componentDidMount() {}
@@ -38,6 +56,10 @@ export default class Index extends Component {
       url: "/pages/driver/index",
     });
   }
+
+  handleChange(val) {
+    console.log("something has been changed:", val);
+  }
   render() {
     return (
       <View className="index">
@@ -58,8 +80,48 @@ export default class Index extends Component {
           rightFirstIconType="bullet-list"
           rightSecondIconType=""
         />
-        <View className="sheet-span">
-          <Text>请点击上面的按钮进行交货单确认操作</Text>
+        <Text className="form-title">货运单详细信息</Text>
+        <View className="sheet-info-span">
+          <Text className="form-caption">
+            运单状态： <Text className="form-hilite">尚未到达</Text>
+          </Text>
+          <Text className="form-caption">运单编号：11234568</Text>
+          <Text className="form-caption">发行时间：2020/06/12 17:18:20</Text>
+          <Text className="form-caption">往来单位：国美电器内蒙古有限公司</Text>
+          <Text className="form-caption">联系电话：0471-9387793</Text>
+          <Text
+            style={{
+              color: "#f5f5f5",
+              height: "33px",
+              margin: "0 auto",
+            }}
+          >
+            ---
+          </Text>
+          <Text className="form-caption">物流中心：01（北京物流中心）</Text>
+          <Text className="form-caption">接货单位：0020 呼和浩特国美电器</Text>
+          <Text className="form-caption">车辆编号：京A 332238（张强）</Text>
+          <AtList>
+            <AtListItem
+              onClick={() => {
+                this.setState({ checked: !this.state.checked });
+              }}
+              title="M09BJZ011288-1-1bj-1（样机）"
+              note="GT-S25NPD.CASWPLGD"
+              iconInfo={{
+                prefixClass: "fa",
+                value: this.state.checked ? "check-square-o" : "square-o",
+                size: "25",
+                color: "#666",
+              }}
+            ></AtListItem>
+          </AtList>
+          <AtButton className="home-button" formType="submit">
+            确认到达
+          </AtButton>
+          <AtButton className="home-button" formType="reset">
+            取消
+          </AtButton>
         </View>
       </View>
     );

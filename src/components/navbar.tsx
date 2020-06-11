19d5120a047ebd07173d57166eeaa0f925bdece0
terry <blueterry@yahoo.com>
2020-06-12 07:33:31 +0800

    change to hook

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,10 @@
-import Taro, { useState } from "@tarojs/taro";
-import { AtNavBar } from "taro-ui";
+import Taro, { useState } from '@tarojs/taro';
+import { AtNavBar } from 'taro-ui';
 
 export default function NavBar(props) {
   return (
     <AtNavBar
+      customStyle={{ backgroundColor: '#a50034', color: '#ffffff' }}
       onClickRgIconSt={this.props.handleClick}
       onClickRgIconNd={this.props.handleClick}
       onClickLeftIcon={this.props.handleClick}
@@ -12,10 +13,10 @@ export default function NavBar(props) {
       leftText=""
       fixed={true}
       leftIconType={{
-        prefixClass: "fa",
-        value: "truck",
-        size: "30",
-        color: "#fff",
+        prefixClass: 'fa',
+        value: 'truck',
+        size: '30',
+        color: '#fff',
       }}
       rightFirstIconType="bullet-list"
       rightSecondIconType=""
--- src/pages/driver/index.tsx
+++ src/pages/driver/index.tsx
@@ -1,17 +1,11 @@
-import Taro, { useState } from "@tarojs/taro";
-import { View, Text, Image, Picker } from "@tarojs/components";
-import { AtForm, AtInput, AtButton, AtList, AtListItem } from "taro-ui";
-import "./index.scss";
-import NavBar from "../../components/navbar";
-import User from "src/models/user";
+import Taro, { useState } from '@tarojs/taro';
+import { View, Text, Image, Picker } from '@tarojs/components';
+import { AtForm, AtInput, AtButton, AtList, AtListItem } from 'taro-ui';
+import './index.scss';
+import NavBar from '../../components/navbar';
+import User from 'src/models/user';
 
-export const TruckTypes: string[] = [
-  "微型面包车",
-  "面包车",
-  "厢式货车2.5吨",
-  "厢式货车5吨",
-  "厢式货车10吨",
-];
+export const TruckTypes: string[] = ['微型面包车', '面包车', '厢式货车2.5吨', '厢式货车5吨', '厢式货车10吨'];
 
 export interface DriverStates {
   //driver: User;
@@ -21,74 +15,58 @@ export interface DriverStates {
   truckType: number;
 }
 
-// const handleChange = (val, type) => {
-//   switch (val) {
-//     case "userName":
-//       this.setState({ userName: type });
-//       break;
-//     case "cellphone":
-//       this.setState({ cellphone: type });
-//       break;
-//     case "plateNum":
-//       this.setState({ plateNum: type });
-//       break;
-//     case "truckType":
-//       this.setState({ truckType: parseInt(type.detail.value) });
-//       break;
-//     default:
-//       console.log("something has been changed:", val, type);
-//       break;
-//   }
-//   console.log("this.state:", this.state);
-// };
-
 export default function Index(props) {
-  const [userName, setUserName] = useState("1");
-  const [cellphone, setCellphone] = useState("2");
-  const [plateNum, setPlateNum] = useState("3");
+  const [userName, setUserName] = useState('1');
+  const [cellphone, setCellphone] = useState('2');
+  const [plateNum, setPlateNum] = useState('3');
   const [truckType, setTruckType] = useState(1);
 
-  const handleChange(target, newVal){
-    switch (val) {
-          case "userName":
-            this.setState({ userName: type });
-            break;
-          case "cellphone":
-            this.setState({ cellphone: type });
-            break;
-          case "plateNum":
-            this.setState({ plateNum: type });
-            break;
-          case "truckType":
-            this.setState({ truckType: parseInt(type.detail.value) });
-            break;
-          default:
-            console.log("something has been changed:", val, type);
-            break;
+  function handleChange(target: string, newVal) {
+    console.log('handleChange:', target, newVal);
+    switch (target) {
+      case 'userName':
+        setUserName(newVal);
+        console.log('userName:', userName);
+        break;
+      case 'cellphone':
+        setCellphone(newVal);
+        break;
+      case 'plateNum':
+        setPlateNum(newVal);
+        break;
+      case 'truckType':
+        setTruckType(parseInt(newVal.detail.value));
+        break;
+      default:
+        console.log('something has been changed:', target, newVal);
+        break;
     }
-        console.log("this.state:", this.state);
+  }
+
+  function onFormSubmit(event) {
+    console.log('submit:', event);
+    console.log('userName:', userName);
+    console.log('cellphone:', cellphone);
+    console.log('plateNum:', plateNum);
+    console.log('truckType:', truckType, TruckTypes[truckType]);
   }
 
   return (
     <View className="index">
       <NavBar
         handleClick={() => {
-          console.log("click", this.state);
+          console.log('click', this.state);
         }}
       />
       <Image
         mode="scaleToFill"
         style="top:0;left:0; width:100%; height: 100%; position:fixed; background-size: 100%, 100%; z-index: -1"
-        src="../../assets/img/back.png"
-      ></Image>
+        src="../../assets/img/back.png"></Image>
       <View className="user-info-span">
         <Text className="form-title">成为注册司机</Text>
         <Text className="form-caption">请输入您的信息</Text>
-        <Text className="form-comment">
-          本系统只对LG注册司机开放，
-          请输入您的个人信息提交管理员审核。审核通过后，您就可以访问相关功能。
-        </Text>
-        <AtForm className="form-userinfo">
+        <Text className="form-comment">本系统只对LG注册司机开放， 请输入您的个人信息提交管理员审核。审核通过后，您就可以访问相关功能。</Text>
+        <AtForm className="form-userinfo" onSubmit={onFormSubmit.bind(this)}>
           <AtInput
             className="input-text"
             name="userName"
@@ -96,7 +74,7 @@ export default function Index(props) {
             type="text"
             placeholder="请输入您的姓名"
             value={userName}
-            onChange={setUserName}
+            onChange={handleChange.bind(this, 'userName')}
           />
           <AtInput
             className="input-text"
@@ -105,7 +83,7 @@ export default function Index(props) {
             type="text"
             placeholder="请输入您的手机号码"
             value={cellphone}
-            onChange={setCellphone}
+            onChange={handleChange.bind(this, 'cellphone')}
           />
           <AtInput
             className="input-text"
@@ -114,15 +92,9 @@ export default function Index(props) {
             type="text"
             placeholder="请输入您的车牌号"
             value={plateNum}
-            onChange={handleChange.bind(this, "plateNum")}
+            onChange={handleChange.bind(this, 'plateNum')}
           />
-          <Picker
-            className="picker"
-            mode="selector"
-            range={TruckTypes}
-            value={truckType}
-            onChange={handleChange.bind(this, "truckType")}
-          >
+          <Picker className="picker" mode="selector" range={TruckTypes} value={truckType} onChange={handleChange.bind(this, 'truckType')}>
             <AtList>
               <AtListItem title="货车类型" />
             </AtList>

338fa938cd872396e58567ede981ea2c202eae6f
terry <blueterry@yahoo.com>
2020-07-01 00:23:29 +0800

    miniprogram autotest

--- wetest/wetest.js
+++ wetest/wetest.js
@@ -1,16 +1,25 @@
-const automator = require("miniprogram-automator");
+const automator = require('miniprogram-automator');
 
 automator
   .launch({
-    cliPath: "F:/Devtools/wxTools/wdt/cli",
+    //cliPath: "F:/Devtools/wxTools/wdt/cli",
     //cliPath: "F:\\Devtools\\wxTools\\微信web开发者工具\\cli",
-    projectPath: "dist",
+    projectPath: '/Users/terry/desktop/works2020/ars/dist',
+    //wsEndpoint: 'ws://localhost:52364'
   })
   .then(async (weapp) => {
-    const page = await weapp.reLaunch("pages/index/index");
+    console.log('open a automatic test...');
+    const page = await weapp.reLaunch('/pages/index/index');
     await page.waitFor(500);
-    const element = await page.$(".home-title-span");
-    console.log(await element.attribute("class"));
-    await element.tap();
+    const element = await page.$('.home-input input');
+    console.log(await element.attribute('class'));
+    //await element.tap();
+    await element.trigger('input', { vsalue: '1' });
+    //console.log(await element);
+    await page.waitFor(500);
+    const btn = await page.$('.home-button');
+    console.log(await btn.attribute('inner_text'));
+    await btn.tap();
+    await page.waitFor(1500);
     await weapp.close();
   });

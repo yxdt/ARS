3aea3a091b908491d6c11a7259937dda59068934
yxdt <blueterry@yahoo.com>
2020-07-12 20:30:29 +0800

    minor update

---
+++ src/components/tests/loading.test.js
@@ -0,0 +1,11 @@
+import Nerv from "nervjs";
+import { Loading } from "../loading";
+describe("Loading Test", () => {
+  let loading;
+  beforeEach(() => {
+    loading = document.createElement("div");
+    document.body.appendChild(loading);
+  });
+
+  it("Loading component test", () => {});
+});

898ae8c97566f98e4bc0b1e32b403c6cf9b043b4
yxdt <blueterry@yahoo.com>
2020-07-21 16:43:10 +0800

    minor change add navbar clickable function

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,45 +1,45 @@
 import Taro from "@tarojs/taro";
 import { AtNavBar } from "taro-ui";
+import { View } from "@tarojs/components";
+import "./comp.scss";
 
 export default function NavBar(props) {
   let loggedIn = Taro.getStorageSync("roleName").toString().length > 0;
+  function doLoginOut() {
+    if (loggedIn) {
+      Taro.removeStorage({
+        key: "roleName",
+        success: () => {
+          Taro.redirectTo({ url: "/pages/index/index" });
+        },
+      });
+      Taro.removeStorage({ key: "userName" });
+    } else {
+      //not logged in yet
+      Taro.redirectTo({ url: "/pages/user/Login" });
+    }
+  }
   return (
-    <AtNavBar
-      customStyle={{
-        backgroundColor: "#ababab",
-      }}
-      onClickRgIconSt={() => {
-        //consolelog("onClickRgIconSt");
-        if (loggedIn) {
-          Taro.removeStorage({
-            key: "roleName",
-            success: () => {
-              Taro.redirectTo({ url: "/pages/index/index" });
-            },
-          });
-          Taro.removeStorage({ key: "userName" });
-        } else {
-          //not logged in yet
-          Taro.redirectTo({ url: "/pages/user/Login" });
+    <View onClick={doLoginOut}>
+      <AtNavBar
+        onClickRgIconSt={doLoginOut}
+        //onClickRgIconNd={this.props.handleClick}
+        //onClickLeftIcon={this.props.handleClick}
+        title={props.title || (loggedIn ? "点击退出" : "配送中心人员登录")}
+        color="#ffffff"
+        leftText=""
+        fixed={true}
+        rightFirstIconType={
+          props.hideRightIcon
+            ? ""
+            : {
+                prefixClass: "fa",
+                value: props.ricon || (loggedIn ? "sign-out" : "id-badge"),
+                size: "28",
+                color: "#ffffff",
+              }
         }
-      }}
-      //onClickRgIconNd={this.props.handleClick}
-      //onClickLeftIcon={this.props.handleClick}
-      title={props.title || (loggedIn ? "点击退出" : "配送中心人员登录")}
-      color="#ffffff"
-      leftText=""
-      fixed={true}
-      rightFirstIconType={
-        props.hideRightIcon
-          ? ""
-          : {
-              prefixClass: "fa",
-              value: props.ricon || (loggedIn ? "sign-out" : "id-badge"),
-              size: "24",
-              color: "#ffffff",
-            }
-      }
-      rightSecondIconType=""
-    />
+      />
+    </View>
   );
 }
